ef25d989c48481bd5f9bfaed8b17b090bfa354ff
Ahmed Belal <ahmedbelalhashmi@gmail.com>
2018-08-02 01:45:03 +0500

    Test elaboration for public API

--- gatecoin_api/tests/test_public.py
+++ gatecoin_api/tests/test_public.py
@@ -8,11 +8,20 @@ def test_get_currency_pairs():
     response = GatecoinAPI.get_currency_pairs()
     assert (response is not None), 'Response did not deserialize properly'
 
+    assert (response.response_status.message ==
+            'OK'), 'API response not successful'
+    assert (len(response.currency_pairs) > 0), 'Empty currency pairs list'
+
 
 def test_get_market_depth():
     """Test fetching market depth info from REST API"""
     response = GatecoinAPI.get_market_depth('BTCUSD')
     assert (response is not None), 'Response did not deserialize properly'
+
+    assert (response.response_status.message ==
+            'OK'), 'API response not successful'
+    assert (len(response.asks) > 0), 'Empty asks list'
+    assert (len(response.bids) > 0), 'Empty bids list'
 
 
 def test_get_order_book():
@@ -20,8 +29,17 @@ def test_get_order_book():
     response = GatecoinAPI.get_order_book('BTCUSD')
     assert (response is not None), 'Response did not deserialize properly'
 
+    # Disabled for now, v1 does not return response_status for this method
+    # assert (response.response_status.message == 'OK'), 'API response not successful'
+    assert (len(response.asks) > 0), 'Empty asks list'
+    assert (len(response.bids) > 0), 'Empty bids list'
+
 
 def test_get_recent_transactions():
     """Test fetching recent transactions from REST API"""
     response = GatecoinAPI.get_recent_transactions('BTCUSD')
     assert (response is not None), 'Response did not deserialize properly'
+
+    assert (response.response_status.message ==
+            'OK'), 'API response not successful'
+    assert (len(response.transactions) > 0), 'Empty transactions list'
